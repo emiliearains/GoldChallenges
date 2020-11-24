4f27792cb976a3da98ea71a0a6ab95948028b30e
Emilie Rains <emiliearains@gmail.com>
2020-11-24 18:31:54 -0500

    Week 2 Gold Challenge

---
+++ WeekTwoGoldChallenge/Greeter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeekTwoGoldChallenge
+{
+    class Greeter
+    {
+        // Method that takes in a name and says hello to the name
+        public string TakeNameSayHello(string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+        // Method that outputs a farewell to the name
+        public string SaysFarewellToName(string name)
+        {
+            return $"Adios, {name}!";
+        }
+
+        // Method that gives time specific salutation
+        public string TimeGreetingToName(string name)
+        {
+            DateTime currTime = DateTime.Now;
+            string returnString = "";
+            if (currTime.Hour < 12)
+            {
+                returnString = $"Good morning, {name}.";
+            }
+            else if (currTime.Hour >= 12 && currTime.Hour < 17)
+            {
+                returnString = $"Good afternoon, {name}.";
+            }
+            else if (currTime.Hour >= 17 && currTime.Hour < 21)
+            {
+                returnString = $"Good evening, {name}.";
+            }
+            else
+            {
+                returnString = $"Good night, {name}.";
+            }
+            return returnString;
+        }
+
+    }
+}
---
+++ WeekTwoGoldChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WeekTwoGoldChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // New up instance of Greeter class -- call all three methods from Greeter
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Greeter myGreeter = new Greeter();
+            Console.WriteLine(myGreeter.TakeNameSayHello(name));
+            Console.WriteLine(myGreeter.SaysFarewellToName(name));
+            Console.WriteLine(myGreeter.TimeGreetingToName(name));
+        }
+    }
+}
